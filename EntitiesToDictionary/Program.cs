2a8f2f8f1669c101ded40f8458e7c1ce23352cfa
Darren Neimke <darren.neimke@live.com>
2017-11-04 13:55:56 +1030

    Add project files.

---
+++ EntitiesToDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntitiesToDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
